814656b58dac89bcdc21d1ffb0ee8e30d795e072
Wercton Barbosa <werctonmatheus@gmail.com>
2022-10-05 16:13:41 -0300

    Bugfix: validando códigos hexadecimais

--- aluraflix/serializer.py
+++ aluraflix/serializer.py
@@ -1,3 +1,4 @@
+from re import search
 from rest_framework import serializers
 from aluraflix.models import Video, Categoria
 
@@ -6,6 +7,12 @@ class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
+        
+    def validate_cor(self, cor):
+        if not search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', cor):
+            raise serializers.ValidationError("Código hexadecimal inválido.")
+        else:
+            return cor
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
