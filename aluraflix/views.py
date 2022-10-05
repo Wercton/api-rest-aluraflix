1c4bf580ec96a32bf94b7826a4eb4368d9bd81c9
Wercton Barbosa <werctonmatheus@gmail.com>
2022-10-04 23:44:20 -0300

    Feature: adicionado autenticação para acesso da API

--- aluraflix/views.py
+++ aluraflix/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 from aluraflix.models import Video
 from aluraflix.serializer import VideoSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class VideosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os vídeos"""
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
